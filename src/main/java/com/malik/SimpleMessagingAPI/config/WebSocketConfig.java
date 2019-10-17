6665d9ec685c40e7d9e0aadcf2cbc36b1081ed7c
Malik Fajar Mubarok <malik_fm@indonesiaindicator.com>
2019-10-17 17:23:20 +0700

    add comment

--- src/main/java/com/malik/SimpleMessagingAPI/config/WebSocketConfig.java
+++ src/main/java/com/malik/SimpleMessagingAPI/config/WebSocketConfig.java
@@ -10,11 +10,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    // register the endpoint
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/message-websocket").withSockJS();
     }
 
+    /*
+    configure the message broker, set the destination prefix
+    prefix /topic can be overriden via @SendTo annotation in streamMessage() method
+    */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
--- src/main/java/com/malik/SimpleMessagingAPI/controller/MessageController.java
+++ src/main/java/com/malik/SimpleMessagingAPI/controller/MessageController.java
@@ -18,24 +18,33 @@ public class MessageController {
     private MessageModel message;
     private List<MessageModel> listMessages = new ArrayList<>();
 
+    /**
+     * if a message is sent to /app/message then the streamMessage() method is called
+     * the return value is broadcast to all subscribers to /topic/message
+     * prefix /app and /topic is defined in configureMessageBroker() method in WebSocketConfig.class
+     * @return message object -> value is set by saveMessage() method
+     */
     @MessageMapping("/message")
     public MessageModel streamMessage() throws Exception {
-        Thread.sleep(300); // simulated delay
         return message;
     }
 
+    // get all previously sent messages
     @GetMapping("")
     public Iterator<MessageModel> getAllMessages() {
         return listMessages.iterator();
     }
 
+    /*
+    send message then save it to list
+    message object is returned
+    */
     @PostMapping("")
     public MessageModel saveMessage(MessageModel messageParam) {
         message = new MessageModel();
         message.setId(atomicInteger.getAndIncrement());
         message.setText(messageParam.getText());
         listMessages.add(message);
-
 
         return message;
     }
